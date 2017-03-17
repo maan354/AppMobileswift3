4467a8ad5ee306e684a70a151f83f89272c2452a
NDIAYE <ndiaye@mbp-de-ndiaye.boiscolombes.fr.ibm.com>
2017-03-17 14:24:44 +0100

    contraints and  UIpikkerView

--- AppMobileswift3/ViewController.swift
+++ AppMobileswift3/ViewController.swift
@@ -8,18 +8,45 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController ,UIPickerViewDelegate,UIPickerViewDataSource{
+    
+    @IBOutlet weak var statesPickerBtn: UIButton!
+    @IBOutlet weak var statesPicker: UIPickerView!
+    var  Country = ["Senegal","France","UK","Mali","USA","Nigeria","Italie","Bresil","Russie","chine" ]
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        self.statesPicker.dataSource = self
+        self.statesPicker.delegate = self
+        statesPicker.isHidden = true
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        
     }
 
+    @IBAction func statesBtnPress(_ sender: Any) {
+        statesPicker.isHidden = false
+        
+    }
+//    required
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+//    required
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return Country.count
+    }
+//    optional
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return Country[row]
+    }
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        statesPickerBtn.setTitle(Country[row], for: .normal)
+        statesPicker.isHidden = true
+    }
 
 }
 
